676e3d6cc5d593d54445ad8b2fe7bc9953a4630b
George Jose <george.jose.a@gmail.com>
2022-07-04 12:59:36 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 brew install vim neovim
 
 mkdir -p "$HOME/.config"
-ln -s ./nvim "$HOME/.config/"
-ln -s ./.vimrc "$HOME/"
-ln -s ./.vim "$HOME/"
+ln -s "$PWD/nvim" "$HOME/.config/"
+ln -s "$PWD/.vimrc" "$HOME/"
+ln -s "$PWD/.vim" "$HOME/"
 
 vim -c ':PlugInstall' -c ':qa!'
 nvim -c ':PlugInstall' -c ':qa!'
